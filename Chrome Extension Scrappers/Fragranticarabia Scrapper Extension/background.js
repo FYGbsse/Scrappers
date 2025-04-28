1d0dec19354834db6195eaad5cbba3479689cbc0
Sajeel Shafqat <sajeelshafqat1@gmail.com>
2025-04-28 14:28:11 +0500

    Data for fragnitie

--- Chrome Extension Scrappers/Fragranticarabia Scrapper Extension/background.js
+++
@@ -1,5 +0,0 @@
-chrome.cookies.getAll({domain: "https://www.fragranticarabia.com"}, function(cookies) {
-    for(var i=0; i<cookies.length;i++) {
-        chrome.cookies.remove({url: "http://https://www.fragranticarabia.com" + cookies[i].path, name: cookies[i].name}, function(deleted_cookie) { console.log(deleted_cookie);});
-    }
-});
